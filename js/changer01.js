43e802c705a72b3482ec965c30e2b59ed4d3a9c1
aimee rivers <aimee@aimeerivers.com>
2015-09-18 13:55:39 +0100

    Neater code, object oriented cards, flip method, update dom...

--- js/changer01.js
+++ js/changer01.js
@@ -1,29 +1,32 @@
-function change(elmnt) {
-    switch(elmnt.innerHTML){
-	    case "A":
-	        elmnt.innerHTML = "2";
-	        break;
-	    case "2":
-	        elmnt.innerHTML = "A";
-	        break;
-	    case "N":
-	        elmnt.innerHTML = "7";
-	        break;
-	    case "7":
-	        elmnt.innerHTML = "N";
-	        break;
-	    case "3":
-	        elmnt.innerHTML = "P";
-	        break;
-	    case "P":
-	        elmnt.innerHTML = "3";
-	        break;
-	    case "4":
-	        elmnt.innerHTML = "E";
-	        break;
-	    case "E":
-	        elmnt.innerHTML = "4";
-	        break;
-	    }
-
+var Card = function (front, back) {
+	this.currentSide = front;
+	this.back = back;
+	this.front = front;
+	this.flip = function () {
+		this.currentSide = (this.currentSide == this.front) ?
+			this.back : this.front;
+	};
+	this.draw = function(element) {
+		element.innerHTML = this.currentSide;
+	};
 }
+
+var change = function(currentDomCard, card) {
+	return function() {
+		card.flip();
+		card.draw(currentDomCard);
+	}
+}
+
+var cards = [
+	new Card('A', '2'),
+	new Card('N', '7'),
+	new Card('3', 'P'),
+	new Card('4', 'E')
+]
+
+for (var i = 0; i < cards.length; i++) {
+	var currentDomCard = document.getElementsByClassName('card')[i];
+	cards[i].draw(currentDomCard);
+	currentDomCard.addEventListener('click', change(currentDomCard, cards[i]));
+};
