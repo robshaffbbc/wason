e2ff0c4d05456d52019f22f6f7e8012c3f825921
aimee rivers <aimee@aimeerivers.com>
2015-09-21 12:21:05 +0100

    Move the card definition into the page.

--- js/changer01.js
+++
@@ -1,8 +0,0 @@
-var cards = [
-	Object.create(card).init(['A', '2']),
-	Object.create(card).init(['N', '7']),
-	Object.create(card).init(['3', 'P']),
-	Object.create(card).init(['4', 'E'])
-];
-
-drawCards(cards);
